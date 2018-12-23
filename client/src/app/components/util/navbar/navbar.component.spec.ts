977914648ec751cda044ba98361c23e5fed99080
Sloan Gwaltney <gwaltneys2@winthrop.edu>
2018-12-22 21:50:30 -0500

    added navbar

--- client/src/app/components/util/navbar/navbar.component.spec.ts
+++ client/src/app/components/util/navbar/navbar.component.spec.ts
@@ -5,6 +5,7 @@ import { NavbarComponent } from './navbar.component';
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let navbarEl: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +17,22 @@ describe('NavbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
+    navbarEl = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle users dropdown', () => {
+    const userDropdownToggleEl: HTMLElement = navbarEl.querySelector('#users-dropdown-toggle');
+    const userDropdown: HTMLElement = navbarEl.querySelector('#users-dropdown');
+    expect(userDropdownToggleEl).toBeTruthy();
+    expect(userDropdown).toBeTruthy();
+    userDropdownToggleEl.click();
+    fixture.detectChanges();
+    expect(component.UsersDropdownOpen).toBeTruthy();
+    expect(userDropdown.className).toBe('dropdown-menu show');
   });
 });
