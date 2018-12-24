87a12ab97b84277940ccaffd6ee6cbde068c98da
Sloan Gwaltney <gwaltneys2@winthrop.edu>
2018-12-24 15:41:33 -0500

    password-card init

---
+++ client/src/app/components/util/password-card/password-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-card',
+  templateUrl: './password-card.component.html',
+  styleUrls: ['./password-card.component.css']
+})
+export class PasswordCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
