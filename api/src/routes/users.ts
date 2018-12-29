8da9cf48ba2cf3a1627444da5ab69a307481807f
WorldOfCode22 <gwaltneys2@winthrop.edu>
2018-12-29 16:14:06 -0500

    added login route

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -9,7 +9,7 @@ const userRouter = Router();
 userRouter.post("/", (req: IRequest, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
-    Validator.validateUsername(username).subscribe(
+    Validator.validateUsername(username, true).subscribe(
         () => null,
         (err) => { req.error = err; return next(); },
         () => {
@@ -21,6 +21,31 @@ userRouter.post("/", (req: IRequest, res, next) => {
                         () => null,
                         (err) => { req.error = err; return next(); },
                         () => { res.status(200).json({message: "registered"}); },
+                    );
+                },
+            );
+        },
+    );
+});
+
+userRouter.post("/login", (req: IRequest, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    Validator.validateUsername(username, false).subscribe(
+        () => null,
+        (err) => { req.error = err; return next(); },
+        () => {
+            Validator.validatePassword(password).subscribe(
+                () => null,
+                (err) => { req.error = err; return next(); },
+                () => {
+                    User.getUserAndCompare({username, password}).subscribe(
+                        (user) => {
+                            if (req.session) { req.session.id = user.id; }
+                            res.status(200).json({message: "logged in"});
+                        },
+                        (err) => { req.error = err; return next(); },
+                        () => null,
                     );
                 },
             );
--- api/test/users.js
+++ api/test/users.js
@@ -4,14 +4,16 @@ const expect = chai.expect;
 const chaiHttp = require('chai-http');
 const { User } = require("../build/models/user-model");
 const { Validator } = require("../build/classes/validator");
+const { hashSync } = require('bcryptjs');
 chai.use(chaiHttp);
 
 describe("register /api/users post", function() {
     let server;
+    let application;
 
     before( function(done) {
-        const Application = new app.Application(() => {
-            server = Application.App
+        application = new app.Application(() => {
+            server = application.App
             done();
         });
     });
@@ -106,4 +108,132 @@ describe("register /api/users post", function() {
             done();
         });
     });
+
+    after(function(done) {
+        application.Server.close();
+        done();
+    });
 })
+
+describe("login /api/users/login post", function() {
+    let server;
+    let newUser = {
+        username: "t".repeat(Validator.username.max),
+        hash: hashSync("T".repeat(Validator.password.max -1 ) + "#"),
+        password: "T".repeat(Validator.password.max -1 ) + "#"
+    }
+
+    before( function(done) {
+        const Application = new app.Application(() => {
+            server = Application.App
+            done();
+        });
+    });
+
+    beforeEach(function(done) {
+        User.deleteMany({}, (err) => {
+            if (err) {done(err)}
+            User.create({username: newUser.username, hash: newUser.hash}, (err) => {
+                if (err) {done(err)}
+                done();
+            })
+        });
+    });
+
+    it("returns 200 a message and a cookie on valid input", function(done) {
+        chai.request(server).post("/api/users/login").send(
+            {username: newUser.username, password: newUser.password}
+        ).end((err, res) => {
+            if (err) {done(err)}
+            expect(res.status).to.eql(200, "invalid status code");
+            expect(res.body).to.be.an("object", "no body provided")
+            expect(res.body).to.have.a.property("message");
+            expect(res).to.have.header("Set-Cookie");
+            done();
+        });
+    });
+
+    it("returns 400 and a ValidationError if username to short", function(done) {
+        chai.request(server).post("/api/users/login").send(
+            {username: "t".repeat(Validator.username.min - 1), password: newUser.password}
+        ).end((err, res) => {
+            if (err) {done(err)}
+            expect(res.status).to.eql(400, "invalid status code");
+            expect(res.body).to.be.an("object", "no body provided")
+            expect(res.body).to.have.a.property("ValidationError");
+            done();
+        });
+    });
+
+    it("returns 400 and a ValidationError if username to long", function(done) {
+        chai.request(server).post("/api/users/login").send(
+            {username: "t".repeat(Validator.username.max + 1), password: newUser.password}
+        ).end((err, res) => {
+            if (err) {done(err)}
+            expect(res.status).to.eql(400, "invalid status code");
+            expect(res.body).to.be.an("object", "no body provided")
+            expect(res.body).to.have.a.property("ValidationError");
+            done();
+        });
+    });
+
+    it("returns 400 and a ValidationError if password to short", function(done) {
+        chai.request(server).post("/api/users/login").send(
+            {username: newUser.username, password: "T".repeat(Validator.password.min - 2) + "$"}
+        ).end((err, res) => {
+            if (err) {done(err)}
+            expect(res.status).to.eql(400, "invalid status code");
+            expect(res.body).to.be.an("object", "no body provided")
+            expect(res.body).to.have.a.property("ValidationError");
+            done();
+        });
+    });
+
+    it("returns 400 and a ValidationError if password to long", function(done) {
+        chai.request(server).post("/api/users/login").send(
+            {username: newUser.username, password: "T".repeat(Validator.password.max) + "$"}
+        ).end((err, res) => {
+            if (err) {done(err)}
+            expect(res.status).to.eql(400, "invalid status code");
+            expect(res.body).to.be.an("object", "no body provided")
+            expect(res.body).to.have.a.property("ValidationError");
+            done();
+        });
+    });
+
+    it("returns 400 and a ValidationError if password needs more capital letters", function(done) {
+        chai.request(server).post("/api/users/login").send(
+            {username: newUser.username, password: "t".repeat(Validator.password.max - 1) + "$"}
+        ).end((err, res) => {
+            if (err) {done(err)}
+            expect(res.status).to.eql(400, "invalid status code");
+            expect(res.body).to.be.an("object", "no body provided")
+            expect(res.body).to.have.a.property("ValidationError");
+            done();
+        });
+    });
+
+    it("returns 400 and a ValidationError if password needs more special chars", function(done) {
+        chai.request(server).post("/api/users/login").send(
+            {username: newUser.username, password: "T".repeat(Validator.password.max)}
+        ).end((err, res) => {
+            if (err) {done(err)}
+            expect(res.status).to.eql(400, "invalid status code");
+            expect(res.body).to.be.an("object", "no body provided")
+            expect(res.body).to.have.a.property("ValidationError");
+            done();
+        });
+    });
+
+    it("returns 400 and a ValidationError if username and password mismatch", function(done) {
+        chai.request(server).post("/api/users/login").send(
+            {username: newUser.username, password: "T".repeat(Validator.password.max - 1) + "$"}
+        ).end((err, res) => {
+            if (err) {done(err)}
+            expect(res.status).to.eql(400, "invalid status code");
+            expect(res.body).to.be.an("object", "no body provided")
+            expect(res.body).to.have.a.property("ValidationError");
+            done();
+        });
+    });
+});
