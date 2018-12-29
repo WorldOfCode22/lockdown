54034fab27a61bfffaf862f3970dab3dd1ff7b44
WorldOfCode22 <gwaltneys2@winthrop.edu>
2018-12-29 02:26:12 -0500

    class for validation (abstraction)

---
+++ api/src/classes/validator.ts
@@ -0,0 +1,96 @@
+import { Observable } from "rxjs";
+import { User, usernameValidator, passwordValidator } from "../models/user-model";
+import { ValidationError } from "./errors/validation-error";
+
+export abstract class Validator {
+    public static username = usernameValidator;
+
+    public static password = passwordValidator;
+
+    public static validateUsername(username: string) {
+        const usernameSpec = Validator.username;
+        return new Observable((observer) => {
+            /// makes sure username length is valid
+            if (username.length >= usernameSpec.min && username.length <= usernameSpec.max ) {
+                /// see if username is not already taken
+                User.findOne({username}, (err, doc) => {
+                    /// catch mongo error
+                    if (err) {
+                        observer.error(err);
+                        observer.unsubscribe();
+                        /// username taken
+                    } else if (doc) {
+                        observer.error(
+                            new ValidationError("username taken"),
+                        );
+                        observer.unsubscribe();
+                        /// username valid
+                    } else {
+                        observer.complete();
+                        observer.unsubscribe();
+                    }
+                });
+            } else {
+                /// error if length is invalid
+                observer.error(
+                    new ValidationError(
+                        `username must have a length between ${usernameSpec.min} and ${usernameSpec.max}`),
+                );
+            }
+        });
+    }
+
+    public static validatePassword(password: string) {
+        const passSpec = Validator.password;
+        return new Observable((observer) => {
+            if (password.length >= passSpec.min && password.length <= passSpec.max) {
+                /// check for minimum number of uppercase chars
+                if (Validator.isEnoughUpperCase(password, passSpec.capLetters || 0)) {
+                    /// check for minimum number of special chars
+                    if (Validator.isEnoughSpecialChars(password, passSpec.specialChars || 0)) {
+                        observer.complete();
+                        observer.unsubscribe();
+                    } else {
+                        observer.error(
+                            new ValidationError(`password must have at least ${passSpec.specialChars} special chars`),
+                        );
+                        observer.unsubscribe();
+                    }
+                } else {
+                    observer.error(new ValidationError(`password must have ${passSpec.capLetters} capital letters`));
+                    observer.unsubscribe();
+                }
+            } else {
+                observer.error(
+                    new ValidationError(`Password must have a length between ${passSpec.min} and ${passSpec.max} `),
+                );
+                observer.unsubscribe();
+            }
+        });
+    }
+
+    // method to see if enough upper case chars are present to pass validation
+    public static isEnoughUpperCase(str: string, minUpperCaseChars: number) {
+        let actualUpperCaseChars = 0;
+        let valid = false;
+        /// loop through chars in  string
+        for (const char of str) {
+            if (char.toUpperCase() === char) { actualUpperCaseChars++; }
+            if (actualUpperCaseChars >= minUpperCaseChars) { valid = true; break; }
+        }
+        return valid;
+    }
+
+    // method to see if enough special chars are present to pass validation
+    public static isEnoughSpecialChars(str: string, minSpecialChars: number) {
+        const specialChars = "!@#$%^&*()_-+={[]}:;.>,</`~";
+        let actualSpecialChars = 0;
+        let valid = false;
+        /// loop through chars of injected string
+        for (const char of str) {
+            if (specialChars.indexOf(char) > -1) { actualSpecialChars++; }
+            if (actualSpecialChars >= minSpecialChars) { valid = true; break; }
+        }
+        return valid;
+    }
+}
