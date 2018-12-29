54faeec9a38a2222f42cf61932c7a8f9e3aa0352
WorldOfCode22 <gwaltneys2@winthrop.edu>
2018-12-29 02:27:22 -0500

    added typings

--- api/src/interfaces.ts
+++ api/src/interfaces.ts
@@ -1,6 +1,36 @@
+import {Document, Model} from "mongoose";
+import { Request } from "express";
+import { Observable } from "rxjs";
+
 export interface IConfig {
     port: string;
     mongoUri: string;
+}
+
+export interface IValidator {
+    min: number;
+    max: number;
+    capLetters?: number;
+    specialChars?: number;
+}
+
+export interface IRequest extends Request {
+    error?: Error;
+}
+
+export interface ICandidateUser {
+    username: string;
+    password: string;
+}
+
+export interface IUser {
+    username: string;
+    hash: string;
+}
+
+export interface IUserDoc extends Document, IUser {}
+export interface IUserModel extends Model<IUserDoc> {
+    hashAndSave: (user: ICandidateUser) => Observable<void>;
 }
 
 export type ErrorOnlyCallback = (error?: Error) => any;
