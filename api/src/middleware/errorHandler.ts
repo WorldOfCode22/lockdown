67a995fd431eb8dead96fd03884f01058b3f1c17
WorldOfCode22 <gwaltneys2@winthrop.edu>
2018-12-29 16:13:19 -0500

    added condtion to check all other errors and send user 500

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -6,5 +6,8 @@ export function errorHandler(req: IRequest, res: Response, next: NextFunction) {
     if (!req.error) { return next(); }
     if (req.error instanceof ValidationError) {
         res.status(400).json({ValidationError: req.error.message});
+    } else {
+        console.log(req.error);
+        res.status(500).json({UnexpectedError: "An unexpected error has ocurred please try again in a few minutes"});
     }
 }
