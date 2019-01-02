bc27878ffe2d97091daa6204649916eeec93ace0
WorldOfCode22 <gwaltneys2@winthrop.edu>
2019-01-02 02:37:00 -0500

    new client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,31 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ClipboardModule } from 'ngx-clipboard';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/util/navbar/navbar.component';
-import { LoginComponent } from './components/routes/login/login.component';
-import { MockApiService } from './mocks/services/mock-api.service';
-import { PasswordKeeperComponent } from './components/routes/password-keeper/password-keeper.component';
-import { PasswordCardComponent } from './components/util/password-card/password-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    LoginComponent,
-    PasswordKeeperComponent,
-    PasswordCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    ClipboardModule
+    AppRoutingModule
   ],
-  providers: [MockApiService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
